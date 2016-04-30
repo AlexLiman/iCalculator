68ba893bf1f8af9893f75ee5a98e9d2227506f3b
AlexLiman <limanalex@mail.ru>
2016-04-30 15:36:15 +0300

    Initial Commit

---
+++ iCalculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iCalculator
+//
+//  Created by Alex on 30.04.16.
+//  Copyright © 2016 com.alexliman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iCalculator
+//
+//  Created by Alex on 30.04.16.
+//  Copyright © 2016 com.alexliman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
